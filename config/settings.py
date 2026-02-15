83c98ab59e16bcf1fdc2247042d09059fd1f23e5
rasoulazizi <rasoulazizi@outlook.com>
2026-02-15 19:23:37 +0330

    config redis on project

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,20 @@ DATABASES = {
     }
 }
 
+# redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
 # custom user
 
 AUTH_USER_MODEL = 'accounts.User'
