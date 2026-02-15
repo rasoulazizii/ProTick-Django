70d3e93cacbda756c3c378763fee8778c8e0d1eb
rasoulazizi <rasoulazizi@outlook.com>
2026-02-15 19:33:05 +0330

    (event): create Organizer model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Organizer(models.Model):
+    user = models.OneToOneField(User)
+    company_name = models.CharField(max_length=220)
+    bio = models.TextField(blank=True, null=True)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.company_name
+
