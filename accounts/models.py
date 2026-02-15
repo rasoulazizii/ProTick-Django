5d1eb988bd995547180403f95d004d109c1bb23b
rasoulazizi <rasoulazizi@outlook.com>
2026-02-15 19:30:23 +0330

    (accounts): add is_organizer, is_customer and phone number

--- accounts/models.py
+++ accounts/models.py
@@ -26,6 +26,9 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
+    is_organizer = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)
+    phone_number = models.CharField(max_length=15, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
